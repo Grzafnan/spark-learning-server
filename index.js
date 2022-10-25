39b0c8c26fc1c55733533200bcbf8eec1cd70de0
Grzafnan <Grzafnan>
2022-10-25 14:02:34 +0600

    create categories api

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const Port = process.env.PORT || 5000;
+
+app.use(cors())
+
+
+const categories = require('./data/categories.json')
+
+app.get('/', (req, res) => {
+  res.send('Running API...')
+})
+
+
+app.get('/courses', (req, res) => {
+  res.send(categories)
+})
+
+
+
+app.listen(Port, (req, res) => {
+  console.log(`Server listening on port ${Port}`)
+})
