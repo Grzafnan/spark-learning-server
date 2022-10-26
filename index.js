42fcaeff7f986831b767e36a5878d69a4a21c196
Grzafnan <Grzafnan>
2022-10-26 13:07:07 +0600

    change image url in two data

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(cors())
 
 
 const categories = require('./data/categories.json')
+const courses = require('./data/courses.json')
 
 app.get('/', (req, res) => {
   res.send('Running API...')
@@ -17,6 +18,28 @@ app.get('/courses-categories', (req, res) => {
   res.send(categories)
 })
 
+app.get('/category/:id', (req, res) => {
+  const id = req.params.id;
+  if (id === '07') {
+    res.send(courses);
+  }
+  else {
+    const category_course = courses.filter(c => c.category_id === id);
+    res.send(category_course);
+  }
+})
+
+app.get('/courses', (req, res) => {
+  res.send(courses)
+})
+
+app.get('/courses/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const selectedCourse = courses.find(c => c._id === id);
+  console.log(selectedCourse);
+  res.send(selectedCourse);
+});
 
 
 app.listen(Port, (req, res) => {
